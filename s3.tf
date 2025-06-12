825f2cab5da558539b22bd4506f740fb9983649f
Suneel Kumar <138564160+suneelprojects@users.noreply.github.com>
2025-06-12 10:48:00 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "one" {
-  bucket = "mustafashaik77.devops.project.bucket"
+  bucket = "suneel-project-v12"
 }
 
 resource "aws_s3_bucket_ownership_controls" "two" {
@@ -25,7 +25,7 @@ status = "Enabled"
 terraform {
 backend "s3" {
 region = "us-east-1"
-bucket = "mustafashaik77.devops.project.bucket"
+bucket = "suneel-project-v12"
 key = "prod/terraform.tfstate"
 }
 }
