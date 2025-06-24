7d402eb975b0b8a196d6c2f38d95fa6447b7744e
Suneel Kumar <138564160+suneelprojects@users.noreply.github.com>
2025-06-24 11:17:24 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -22,10 +22,3 @@ versioning_configuration {
 status = "Enabled"
 }
 }
-terraform {
-backend "s3" {
-region = "us-east-1"
-bucket = "suneel.devops.project.cloud.v1"
-key = "prod/terraform.tfstate"
-}
-}
