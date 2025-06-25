f49cafc74f534eb4c449601f8ea8dae2b978b9ea
kompallyvishnuvardhanreddy <vishnuvardhanreddykompally@gmail.com>
2025-06-25 21:01:27 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,12 +1,10 @@
- resource "aws_s3_bucket" "one" {
+resource "aws_s3_bucket" "one" {
   bucket = "vishnu.monolithic.project.cloud.v9"
 
   lifecycle {
     prevent_destroy = true
     ignore_changes  = [bucket]
   }
-}
-
 }
 
 resource "aws_s3_bucket_ownership_controls" "two" {
@@ -18,22 +16,21 @@ resource "aws_s3_bucket_ownership_controls" "two" {
 
 resource "aws_s3_bucket_acl" "three" {
   depends_on = [aws_s3_bucket_ownership_controls.two]
+  bucket     = aws_s3_bucket.one.id
+  acl        = "private"
+}
 
+resource "aws_s3_bucket_versioning" "four" {
   bucket = aws_s3_bucket.one.id
-  acl    = "private"
-}
-
-resource "aws_s3_bucket_versioning" "three" {
-bucket = aws_s3_bucket.one.id
-versioning_configuration {
-status = "Enabled"
-}
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
 
 terraform {
-backend "s3" {
-region = "us-east-1"
-bucket = "vishnu.monolithic.project.cloud.v9"
-key = "prod/terraform.tfstate"
-}
+  backend "s3" {
+    region = "us-east-1"
+    bucket = "vishnu.monolithic.project.cloud.v9"
+    key    = "prod/terraform.tfstate"
+  }
 }
