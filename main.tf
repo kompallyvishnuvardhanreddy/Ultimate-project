31c24a00c7cb8d41249c6bfa59961f7a532b2dcc
kompallyvishnuvardhanreddy <vishnuvardhanreddykompally@gmail.com>
2025-06-25 19:24:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_launch_template" "web_server_as" {
     image_id           = "ami-000ec6c25978d5999"
     vpc_security_group_ids = [aws_security_group.web_server.id]
     instance_type = "t2.micro"
-    key_name = "shell"
+    key_name = "Kops"
     tags = {
         Name = "DevOps"
     }
@@ -15,7 +15,7 @@ resource "aws_launch_template" "web_server_as" {
   resource "aws_elb" "web_server_lb"{
      name = "web-server-lb"
      security_groups = [aws_security_group.web_server.id]
-     subnets = ["subnet-08cd3eab2b138b380", "subnet-0ae19d717e8ba3ad8"]
+     subnets = ["subnet-062a0b0cc84a41ce5", "subnet-03687650f50429431"]
      listener {
       instance_port     = 8000
       instance_protocol = "http"
